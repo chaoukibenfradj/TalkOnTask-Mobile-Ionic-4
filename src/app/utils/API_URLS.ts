f80237ccd0884642de856dba1fd2228dda112d3a
Chaouki Ben Fredj <chaoukibenfraj@gmail.com>
2019-12-21 20:34:23 +0100

    Cleanup - Merging

--- src/app/utils/API_URLS.ts
+++ src/app/utils/API_URLS.ts
@@ -8,6 +8,5 @@ export const GET_ALL_USER_BY_TYPE = API_URL + 'user/type/';
 export const GET_USER_BY_ID = API_URL + 'user/id/';
 export const GET_MESSAGES = API_URL + 'message/get/';
 export const GET_LAST_MESSAGES = API_URL + 'message/lastMessages/';
-export const API_URL = 'http://localhost:3000/' ;
 export const GET_ALL_PROJECTS = API_URL + 'project/all';
 export const ADD_TASK = API_URL + 'task/addT';
